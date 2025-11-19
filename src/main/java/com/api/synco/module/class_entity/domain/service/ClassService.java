687413e86362e24c4b25b6fc2ea0ad2301d97924
Luca5Eckert <ecklukatv@gmail.com>
2025-11-19 15:01:32 -0300

    #66 feat: adiciona método para atualizar turma

--- src/main/java/com/api/synco/module/class_entity/domain/service/ClassService.java
+++ src/main/java/com/api/synco/module/class_entity/domain/service/ClassService.java
@@ -2,10 +2,15 @@ package com.api.synco.module.class_entity.domain.service;
 
 import com.api.synco.module.class_entity.application.dto.create.CreateClassRequest;
 import com.api.synco.module.class_entity.application.dto.create.CreateClassResponse;
+import com.api.synco.module.class_entity.application.dto.update.UpdateClassRequest;
+import com.api.synco.module.class_entity.application.dto.update.UpdateClassResponse;
 import com.api.synco.module.class_entity.domain.ClassEntity;
+import com.api.synco.module.class_entity.domain.ClassEntityId;
 import com.api.synco.module.class_entity.domain.mapper.ClassMapper;
 import com.api.synco.module.class_entity.domain.port.ClassRepository;
 import com.api.synco.module.class_entity.domain.use_case.CreateClassUseCase;
+import com.api.synco.module.class_entity.domain.use_case.UpdateClassUseCase;
+import com.api.synco.module.course.domain.CourseEntity;
 import com.api.synco.module.course.domain.use_cases.CreateCourseUseCase;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
@@ -16,16 +21,26 @@ public class ClassService {
     private final ClassMapper classMapper;
 
     private final CreateClassUseCase createClassUseCase;
+    private final UpdateClassUseCase updateClassUseCase;
 
-    public ClassService(ClassMapper classMapper, CreateClassUseCase createClassUseCase) {
+    public ClassService(ClassMapper classMapper, CreateClassUseCase createClassUseCase, UpdateClassUseCase updateClassUseCase) {
         this.classMapper = classMapper;
         this.createClassUseCase = createClassUseCase;
+        this.updateClassUseCase = updateClassUseCase;
     }
 
     public CreateClassResponse create(CreateClassRequest createClassRequest, long idUser) {
         ClassEntity classEntity = createClassUseCase.execute(createClassRequest, idUser);
 
         return classMapper.toCreateResponse(classEntity);
+    }
+
+    public UpdateClassResponse update(UpdateClassRequest updateClassRequest, long idCourse, int numberClass, long idUser){
+        ClassEntityId classEntityId = new ClassEntityId(idCourse, numberClass);
+
+        ClassEntity classEntity = updateClassUseCase.execute(updateClassRequest, classEntityId, idUser);
+
+        return classMapper.toUpdateResponse(classEntity);
     }
 
 }
