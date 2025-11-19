dad73562eae9fae1d9b943298dc0b273c2374424
Luca5Eckert <ecklukatv@gmail.com>
2025-11-19 13:56:27 -0300

    #69 feat: adiciona serviço de permissão

---
+++ src/main/java/com/api/synco/module/permission/domain/service/PermissionService.java
@@ -0,0 +1,28 @@
+package com.api.synco.module.permission.domain.service;
+
+import com.api.synco.module.permission.domain.policies.PermissionPolicy;
+import com.api.synco.module.user.domain.enumerator.RoleUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PermissionService {
+
+    private final PermissionPolicy permissionPolicy;
+
+    public PermissionService(PermissionPolicy permissionPolicy) {
+        this.permissionPolicy = permissionPolicy;
+    }
+
+    public boolean canModifyClass(RoleUser roleUser){
+        return permissionPolicy.canModifyClass(roleUser);
+    }
+
+    public boolean canModifyCourse(RoleUser roleUser){
+        return permissionPolicy.canModifyCourse(roleUser);
+    }
+
+    public boolean canModifyUser(RoleUser roleUser){
+        return permissionPolicy.canModifyCourse(roleUser);
+    }
+
+}
