9c1c8a8010467b5d5142dda0d6edda62ca6007d6
Luca5Eckert <ecklukatv@gmail.com>
2025-12-02 19:38:50 -0300

    #84 feat: adiciona entidade de frequencia usuário

--- src/main/java/com/api/synco/module/attendance_user/domain/AttendanceUserEntity.java
+++ src/main/java/com/api/synco/module/attendance_user/domain/AttendanceUserEntity.java
@@ -1,4 +1,61 @@
 package com.api.synco.module.attendance_user.domain;
 
+import com.api.synco.module.period.domain.PeriodEntity;
+import com.api.synco.module.user.domain.UserEntity;
+import jakarta.persistence.*;
+
+@Entity
 public class AttendanceUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PeriodEntity period;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserEntity userEntity;
+
+    private boolean isPresent;
+
+
+    public AttendanceUserEntity() {
+    }
+
+    public AttendanceUserEntity(long id, PeriodEntity period, UserEntity userEntity, boolean isPresent) {
+        this.id = id;
+        this.period = period;
+        this.userEntity = userEntity;
+        this.isPresent = isPresent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public PeriodEntity getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(PeriodEntity period) {
+        this.period = period;
+    }
+
+    public UserEntity getUserEntity() {
+        return userEntity;
+    }
+
+    public void setUserEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    public boolean isPresent() {
+        return isPresent;
+    }
+
+    public void setPresent(boolean present) {
+        isPresent = present;
+    }
+
 }
