53cf482fe4a14bdbaacc362718ca41e9e465b4f2
Luca5Eckert <ecklukatv@gmail.com>
2025-12-05 13:52:01 -0300

    #95 feat: adiciona delete em service

--- src/main/java/com/api/synco/module/class_user/domain/service/ClassUserService.java
+++ src/main/java/com/api/synco/module/class_user/domain/service/ClassUserService.java
@@ -1,10 +1,19 @@
 package com.api.synco.module.class_user.domain.service;
 
+import com.api.synco.module.class_entity.domain.ClassEntityId;
+import com.api.synco.module.class_user.application.dto.get.GetClassUserResponse;
+import com.api.synco.module.class_user.domain.ClassUser;
+import com.api.synco.module.class_user.domain.ClassUserId;
+import com.api.synco.module.class_user.domain.mapper.ClassUserMapper;
 import com.api.synco.module.class_user.domain.use_cases.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClassUserService {
+
+    private final ClassUserMapper classUserMapper;
 
     private final CreateClassUserUseCase createClassUserUseCase;
     private final UpdateClassUserUseCase updateClassUserUseCase;
@@ -12,13 +21,24 @@ public class ClassUserService {
     private final GetClassUserUseCase getClassUserUseCase;
     private final GetAllClassUserUseCase getAllClassUserUseCase;
 
-    public ClassUserService(CreateClassUserUseCase createClassUserUseCase, UpdateClassUserUseCase updateClassUserUseCase, DeleteClassUserUseCase deleteClassUserUseCase, GetClassUserUseCase getClassUserUseCase, GetAllClassUserUseCase getAllClassUserUseCase) {
+    public ClassUserService(ClassUserMapper classUserMapper, CreateClassUserUseCase createClassUserUseCase, UpdateClassUserUseCase updateClassUserUseCase, DeleteClassUserUseCase deleteClassUserUseCase, GetClassUserUseCase getClassUserUseCase, GetAllClassUserUseCase getAllClassUserUseCase) {
+        this.classUserMapper = classUserMapper;
         this.createClassUserUseCase = createClassUserUseCase;
         this.updateClassUserUseCase = updateClassUserUseCase;
         this.deleteClassUserUseCase = deleteClassUserUseCase;
         this.getClassUserUseCase = getClassUserUseCase;
         this.getAllClassUserUseCase = getAllClassUserUseCase;
     }
+
+    public void delete(long idCourse, int classNumber, long idUser, long userAuthenticatedId){
+        ClassEntityId classEntityId = new ClassEntityId(idCourse, classNumber);
+        ClassUserId classUserId = new ClassUserId(idUser, classEntityId);
+
+        deleteClassUserUseCase.execute(classUserId, userAuthenticatedId);
+    }
+
+
+
 
 
 }
