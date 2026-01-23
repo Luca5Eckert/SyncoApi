499ec777344cc2848ffc13d6347323118c040e5f
Luca5Eckert <ecklukatv@gmail.com>
2026-01-23 14:35:10 -0300

    #102 refactor(room): create UserPermissionPolicy

---
+++ src/main/java/com/api/synco/module/user/domain/permission/UserPermissionPolicy.java
@@ -0,0 +1,22 @@
+package com.api.synco.module.user.domain.permission;
+
+import com.api.synco.module.permission.domain.policies.PermissionPolicy;
+import com.api.synco.module.user.domain.enumerator.RoleUser;
+
+public class UserPermissionPolicy implements PermissionPolicy {
+    @Override
+    public boolean canEdit(RoleUser roleUser) {
+        return roleUser.equals(RoleUser.ADMIN);
+    }
+
+    @Override
+    public boolean canCreate(RoleUser roleUser) {
+        return roleUser.equals(RoleUser.ADMIN);
+    }
+
+    @Override
+    public boolean canDelete(RoleUser roleUser) {
+        return roleUser.equals(RoleUser.ADMIN);
+    }
+
+}
