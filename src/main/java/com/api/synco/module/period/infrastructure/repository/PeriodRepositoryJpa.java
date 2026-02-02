9e460e7c0147796b01d90585d73fcf94fc3778e7
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-02-02 16:30:15 -0300

    feat: implement method findAll in PeriodRepositoryAdapter

--- src/main/java/com/api/synco/module/period/infrastructure/repository/PeriodRepositoryJpa.java
+++ src/main/java/com/api/synco/module/period/infrastructure/repository/PeriodRepositoryJpa.java
@@ -2,8 +2,9 @@ package com.api.synco.module.period.infrastructure.repository;
 
 import com.api.synco.module.period.domain.PeriodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PeriodRepositoryJpa extends JpaRepository<PeriodEntity, Long> {
+public interface PeriodRepositoryJpa extends JpaRepository<PeriodEntity, Long>, JpaSpecificationExecutor<PeriodEntity> {
 }
